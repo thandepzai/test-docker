33273893c4afceeec9ac60be04c45871fd7143be
thandepzai <91040602+thandepzai@users.noreply.github.com>
2024-08-21 14:31:56 +0700

    add some

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,5 @@
 export default function HomeView() {
-    return <div className="h-screen w-full bg-blue-200 text-center">Thân đẹp zai</div>
+    return <div className="h-screen w-full bg-blue-200 text-center">
+        Test 2
+    </div>
 }
